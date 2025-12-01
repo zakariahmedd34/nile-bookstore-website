7dd9d0c77aef4cfce9ea2db2a2c2a0782ce5896d
Zakaria <Zakaria.ahmed@gmail.com>
2025-12-01 20:08:04 +0200

    refactor models, add tablenames

--- app.py
+++ app.py
@@ -5,16 +5,15 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 
 def create_app():
-    
+
     app = Flask(__name__, template_folder='templates', static_folder='static')
     sql_server_password = " "
     app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:{sql_server_password}@localhost:3306/bookstore"
 
     db.init_app(app)
-
+    # import later on
     from route import register_routes
     register_routes(app,db)
-
     migrate = Migrate(app,db)
 
     return app
