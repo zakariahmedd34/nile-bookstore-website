55e1df9a1a378c634dd087399b81274272951329
Zakaria <Zakaria.ahmed@gmail.com>
2025-12-20 01:22:05 +0200

    updeate2

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ def create_app(test_config=None):
     # INIT EXTENSIONS
     # =========================
     db.init_app(app)
+    from models import User,Payment,OrderItem,Address,Category,Book,Order,CartItem
     migrate.init_app(app, db)
 
     login_manager = LoginManager()
