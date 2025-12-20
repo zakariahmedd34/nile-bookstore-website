1d0a58fc864f27d693ecb3fab2f9a5939794c5b8
Zakaria <Zakaria.ahmed@gmail.com>
2025-12-20 02:12:22 +0200

    try sqllite

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ migrate = Migrate()
 def create_app(test_config=None):
     load_dotenv()
 
-    app = Flask(__name__, template_folder='templates', static_folder='static')
+    app = Flask(__name__, template_folder="templates", static_folder="static")
 
     # =========================
     # CONFIG
@@ -23,23 +23,9 @@ def create_app(test_config=None):
         database_url = os.getenv("DATABASE_URL")
 
         if database_url:
-            # Railway gives mysql:// but SQLAlchemy needs mysql+pymysql://
-            if database_url.startswith("mysql://"):
-                database_url = database_url.replace(
-                    "mysql://", "mysql+pymysql://", 1
-                )
-
             app.config["SQLALCHEMY_DATABASE_URI"] = database_url
         else:
-            # Local MySQL
-            db_user = os.getenv("MYSQLUSER", "root")
-            db_password = os.getenv("MYSQLPASSWORD", "Password%401234%23")
-            db_host = os.getenv("MYSQLHOST", "localhost")
-            db_name = os.getenv("MYSQLDB", "bookstore")
-
-            app.config["SQLALCHEMY_DATABASE_URI"] = (
-                f"mysql+pymysql://{db_user}:{db_password}@{db_host}:3306/{db_name}"
-            )
+            app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.secret_key = os.getenv("SECRET_KEY", "dev-secret-key")
@@ -48,7 +34,18 @@ def create_app(test_config=None):
     # INIT EXTENSIONS
     # =========================
     db.init_app(app)
-    from models import User,Payment,OrderItem,Address,Category,Book,Order,CartItem
+
+    from models import (
+        User,
+        Payment,
+        OrderItem,
+        Address,
+        Category,
+        Book,
+        Order,
+        CartItem,
+    )
+
     migrate.init_app(app, db)
 
     login_manager = LoginManager()
@@ -59,8 +56,6 @@ def create_app(test_config=None):
     # =========================
     # LOGIN MANAGER
     # =========================
-
-
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
